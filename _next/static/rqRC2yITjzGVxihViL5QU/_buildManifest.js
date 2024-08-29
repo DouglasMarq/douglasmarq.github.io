ff762069ac79df270300992c4f4f03ea631d1416
Douglas Marques Alves <douglas.marques@olxbr.com>
2024-08-29 20:28:35 -0300

    Updates

---
+++ _next/static/rqRC2yITjzGVxihViL5QU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/316-5485a7e8a90501ea.js","static/chunks/pages/index-4eeda8eca1c86ca1.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
