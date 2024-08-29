59b364d916ffa7fa8ca86470c4ba27cce4778154
Douglas Marques Alves <douglas.marques@olxbr.com>
2024-08-29 20:34:01 -0300

    feat: preact -> react

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   output: 'export',
   distDir: 'build',
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
   optimizeFonts: true,
   eslint: {
       ignoreDuringBuilds: true
